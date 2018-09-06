219a09caadb5985196d2d5d9bf4a451278b45e99
Christian Posta <christian.posta@gmail.com>
2018-09-06 14:38:45 -0700

    added more samples for chapter 5

---
+++ forum-spring-boot/src/main/java/com/redhat/cloudnative/catalog/ForumApplication.java
@@ -0,0 +1,12 @@
+package com.redhat.cloudnative.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumApplication.class, args);
+    }
+}
