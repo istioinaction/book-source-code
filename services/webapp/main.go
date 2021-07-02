d7e1eaf3d5abad566a7551916f984c16c85fe764
Rinor Maloku <rinormaloku37@gmail.com>
2021-07-02 13:35:29 +0200

    updated the applicaiton

--- services/webapp/main.go
+++ services/webapp/main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	web.Router("/", &controllers.IndexController{})
-	web.Router("/api/catalogs", &controllers.CatalogController{}, "post:CreateCatalog")
-	web.Router("/api/catalogs", &controllers.CatalogController{}, "*:GetCatalogList")
-	web.Router("/api/catalogs/:id:int", &controllers.CatalogController{}, "*:GetCatalog")
+	web.Router("/api/catalog/items", &controllers.CatalogController{}, "post:CreateCatalog")
+	web.Router("/api/catalog", &controllers.CatalogController{}, "*:GetCatalogList")
+	web.Router("/api/catalog/items/:id:int", &controllers.CatalogController{}, "*:GetCatalog")
 	web.Router("/api/users", &controllers.ForumController{}, "*:GetUsersList")
 	web.BConfig.Log.AccessLogs = true
 	web.Run()
