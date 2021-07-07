6fbec06b116be2a39df98f385dabad071ea96520
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2021-07-07 17:39:20 +0600

    changed using alias k to full kubectl name

--- bin/poll_customer.sh
+++ bin/poll_customer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL=$(k get pod -n istio-system -l istio=ingressgateway -o jsonpath='{.items[0].status.hostIP}'):$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+URL=$(kubectl get pod -n istio-system -l istio=ingressgateway -o jsonpath='{.items[0].status.hostIP}'):$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
 
 while true
 do curl $URL$1
