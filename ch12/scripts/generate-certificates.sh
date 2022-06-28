afbeb1e8498af5e92fef351efdfc57a4faae96ed
rinormaloku <rinormaloku37@gmail.com>
2022-06-28 16:43:54 +0200

    Updated the certificates

--- ch12/scripts/generate-certificates.sh
+++ ch12/scripts/generate-certificates.sh
@@ -20,8 +20,8 @@ step certificate create root.istio.in.action ${cert_dir}/root-cert.pem ${cert_di
   --profile root-ca --no-password --insecure --san root.istio.in.action \
   --not-after 87600h --kty RSA 
 
-step certificate create west.intermediate.istio.in.action ${cert_dir}/west-cluster/ca-cert.pem ${cert_dir}/west-cluster/ca-key.pem --ca ${cert_dir}/root-cert.pem --ca-key ${cert_dir}/root-ca.key --profile intermediate-ca --not-after 8760h --no-password --insecure --san west.intermediate.istio.in.action --kty RSA 
-step certificate create east.intermediate.istio.in.action ${cert_dir}/east-cluster/ca-cert.pem ${cert_dir}/east-cluster/ca-key.pem --ca ${cert_dir}/root-cert.pem --ca-key ${cert_dir}/root-ca.key --profile intermediate-ca --not-after 8760h --no-password --insecure --san east.intermediate.istio.in.action --kty RSA 
+step certificate create west.intermediate.istio.in.action ${cert_dir}/west-cluster/ca-cert.pem ${cert_dir}/west-cluster/ca-key.pem --ca ${cert_dir}/root-cert.pem --ca-key ${cert_dir}/root-ca.key --profile intermediate-ca --not-after 87600h --no-password --insecure --san west.intermediate.istio.in.action --kty RSA 
+step certificate create east.intermediate.istio.in.action ${cert_dir}/east-cluster/ca-cert.pem ${cert_dir}/east-cluster/ca-key.pem --ca ${cert_dir}/root-cert.pem --ca-key ${cert_dir}/root-ca.key --profile intermediate-ca --not-after 87600h --no-password --insecure --san east.intermediate.istio.in.action --kty RSA 
 
 cat ${cert_dir}/west-cluster/ca-cert.pem ${cert_dir}/root-cert.pem > ${cert_dir}/west-cluster/cert-chain.pem
 cat ${cert_dir}/east-cluster/ca-cert.pem ${cert_dir}/root-cert.pem > ${cert_dir}/east-cluster/cert-chain.pem
